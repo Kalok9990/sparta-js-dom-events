fa8c3511dadb127d0152bd68f542dbd34d40e447
Tech-A29 <Tech-A29@Tech-A29s-MacBook-Air.local>
2017-11-01 16:48:31 +0000

    files added

---
+++ index.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function(){
+  console.log("The content is loaded");
+  // write code here
+
+  var myButton = document.getElementById('myButton');
+
+  //First param - type of action
+  //Second param - what will happen (callback)
+
+  // myButton.addEventListener("click", function() {
+  //   console.log("Button was clicked");
+  // })
+
+  myButton.addEventListener("click", handleClick);
+
+  function handleClick(){
+    console.log("Button was clicked via a function");
+  }
+
+  var myForm = document.getElementById("myForm");
+
+  myForm.addEventListener("submit", function(event){
+    console.log("Form was submitted");
+    event.preventDefault();
+  });
+
+  var buttons = document.querySelectorAll(".myButtons");
+  for (var i = 0; i < buttons.length; i++) {
+    var button = buttons[i];
+    button.addEventListener("click", function(event){
+      console.log(this.value + " was clicked.");
+    })
+  }
+//reminder end of parameters
+})
